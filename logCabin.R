7715bf81a92228693ce5574d5aaf79bb540b3ca5
Excel Que <34141973+exsquire@users.noreply.github.com>
2019-12-31 11:41:00 -0800

    Add documentation and grep indices for output

--- logCabin.R
+++ logCabin.R
@@ -19,32 +19,34 @@ logCabin <- function(pathIn, pull = c("Name",
                                        "Max Disk Write",
                                        "Max Disk Read"))
 {
+  #Pull outfiles
   outfiles <- list.files(pathIn, pattern = ".out",full.names = TRUE)
   outlist <- list()
   for(i in seq_along(outfiles)){
     tmp <- readLines(outfiles[i])
+    #grep for strings in 'pull', collapse to modify and multi-match pattern
     reg <- grep(paste("^",pull, sep = "",collapse = " *:|"), tmp)
+    #null out files with no profile data
     if(length(reg) == 0){
       outlist[[i]] <- NULL
       next
     }
-    #Check if matches are sequential
-    #stopifnot(all(abs(diff(reg)) == 1))
-    
-    pos <- reg[1]
-    
-    split <- strsplit(tmp[pos:length(tmp)], ": ")
+    #Format for split on colon, trim whitespace
+    split <- strsplit(tmp[reg], ": ")
     trim <- lapply(split, trimws)
+    #subset second position of split list elements, name them as first position of split list elements
     outlist[[i]] <- setNames(sapply(trim, function(x)x[2]),sapply(trim, function(x)x[1]))
+    #Add additional column using file name - req good cluster naming practices to be useful
     outlist[[i]] <- c("jobID" = gsub("\\.out$","",basename(outfiles[i])),outlist[[i]])
   }
-  
+  #Remove nulls and bind
   outlist[sapply(outlist, is.null)] <- NULL
   res <- do.call("rbind",outlist)
   return(res)
 }
 
-test <- logCabin("test")
-
+#examples
+all <- logCabin("./test")
+some <- logCabin("./test", pull = c("User","Reserved walltime","Used walltime","Mem reserved","Max Mem used"))
 
 
